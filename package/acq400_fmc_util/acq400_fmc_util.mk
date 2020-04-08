08ecc9450be4f32da900cc76464e092333a99f31
Peter Milne <peter.milne@d-tacq.com>
2020-04-08 13:04:01 +0100

    builds form github. NB: the github repo MUST be archived!  On branch acq400_v2020.02 	modified:   package/acq400_fmc_util/acq400_fmc_util.mk

--- package/acq400_fmc_util/acq400_fmc_util.mk
+++ package/acq400_fmc_util/acq400_fmc_util.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-ACQ400_FMC_UTIL_VERSION = 1.0
-ACQ400_FMC_UTIL_SITE = ../ACQ400/TOOLS/fmc-bus-2013-05-release/tools
-ACQ400_FMC_UTIL_SITE_METHOD = local
+ACQ400_FMC_UTIL_VERSION = 1.1
+ACQ400_FMC_UTIL_SITE = $(call github,petermilne,fmctools,V$(ACQ400_FMC_UTIL_VERSION))
+#ACQ400_FMC_UTIL_SITE_METHOD = local
 ACQ400_FMC_UTIL_INSTALL_STAGING = YES
 
 define ACQ400_FMC_UTIL_BUILD_CMDS
@@ -15,7 +15,7 @@ define ACQ400_FMC_UTIL_BUILD_CMDS
 endef
 
 define ACQ400_FMC_UTIL_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/fru-dump-acq $(TARGET_DIR)/bin
+	$(INSTALL) -D -m 0755 $(@D)/tools/fru-dump-acq $(TARGET_DIR)/bin
 endef
 
 $(eval $(generic-package))
